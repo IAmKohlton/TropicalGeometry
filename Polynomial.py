348160c16fad426c690eef133c26391d9303b9e6
IAmKohlton <booth.kohl@gmail.com>
2020-04-30 14:35:50 -0600

    function to evaluate polynomial

--- Polynomial.py
+++ Polynomial.py
@@ -1,4 +1,4 @@
-from Variable import Variable
+import Variable
 
 
 class Polynomial(object):
@@ -6,18 +6,18 @@ class Polynomial(object):
         if input is None:
             self.poly = (None, None, None)
         else:
-            assert isinstance(input, int) or isinstance(input, float) or isinstance(input, Polynomial) or isinstance(input, Variable)
+            assert isinstance(input, int) or isinstance(input, float) or isinstance(input, Polynomial) or isinstance(input, Variable.Variable)
             self.poly = input
 
         if isinstance(self.poly, Polynomial):
             self.vars = self.poly.vars
-        elif isinstance(self.poly, Variable):
+        elif isinstance(self.poly, Variable.Variable):
             self.vars = {self.poly}
         else:
             self.vars = set()
 
     def operate(self, input, op):
-        assert isinstance(input, int) or isinstance(input, float) or isinstance(input, Polynomial) or isinstance(input, Variable)
+        assert isinstance(input, int) or isinstance(input, float) or isinstance(input, Polynomial) or isinstance(input, Variable.Variable)
         if self.poly == (None, None, None):
             return Polynomial(input=input)
         else:
@@ -25,7 +25,7 @@ class Polynomial(object):
             newPoly.poly = (self, op, input)
             if isinstance(input, Polynomial):
                 newPoly.vars = input.vars | self.vars
-            elif isinstance(input, Variable):
+            elif isinstance(input, Variable.Variable):
                 newPoly.vars = {input} | self.vars
             else:
                 newPoly.vars = self.vars
@@ -51,8 +51,26 @@ class Polynomial(object):
         else:
             return "(" + str(self.poly[0]) + self.poly[1] + str(self.poly[2]) + ")"
 
+    def evalRecurse(self, x):
+        if isinstance(self.poly, Variable):
+            return x[self.poly.name]
+        elif isinstance(self.poly, int) or isinstance(self.poly, float):
+            return self.poly
+        elif isinstance(self.poly, tuple):
+            if self.poly[1] == "+":
+                return min(self.poly[0].evalRecurse(x), self.poly[2].evalRecurse(x))
+            if self.poly[2] == "*":
+                return self.poly[0].evalRecurse(x) + self.poly[2].evalRecurse(x)
+
     def eval(self, x):
         if self.poly is (None, None, None):
             return None
-        elif isinstance(self.poly, Variable):
-            return None
+
+        varNameSet = {x.name for x in self.vars}
+        if not isinstance(x, dict):
+            if len(self.vars) != 1:
+                raise Exception("Tried to evaluate polynomial %s with a single variable, but there is more than one variable that needs to be assigned" % str(self))
+        elif varNameSet != x.keys:
+            raise Exception("Didn't assign all of the variables in the polynomial")
+
+        return self.evalRecurse(x)
