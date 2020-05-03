e8c038ed8b6041cb58dde3d25cc64118684af010
IAmKohlton <booth.kohl@gmail.com>
2020-05-03 14:02:51 -0600

    Made tests automated

--- polyTest.py
+++ polyTest.py
@@ -1,58 +1,105 @@
 from Polynomial import Polynomial
 from Variable import Variable
+from random import random
 
+
+def compare(expected, got):
+    """ Compare two strings
+    """
+    if expected != got:
+        print("Test case failed.\nExpected: %s\nBut got: %s" % (expected, got))
+
+
+def compareSet(expected, got):
+    """ Compare two sets by casting elements to strings
+        Made for the q.vars parameter especially
+    """
+    newExpected = set()
+    for el in expected:
+        newExpected.add(str(el))
+    newGot = set()
+    for el in got:
+        newGot.add(str(el))
+    compare(newExpected, newGot)
+
+
+def comparePoly(expected, got):
+    """ Compare if two polynomials are equal based on what they evaluate to
+        This is a rather crude way of doing but it works in most cases.
+        Only checks if function values are equal in range of +100 to -100 of each variable
+    """
+    if expected.vars != got.vars:
+        print("Variable set for expected is: %s\n but the got variable set is: %s")
+    variables = [str(x) for x in expected.vars]
+
+    equal = True
+    for i in range(1000):
+        valueList = [random() * 200 - 100 for var in variables]
+        varDict = {}
+        for var, value in zip(variables, valueList):
+            varDict[var] = value
+        if expected.eval(varDict) != got.eval(varDict):
+            equal = False
+    if not equal:
+        print("The given polynomials are not equal")
+
+
+# make sure that the basic polynomial contructor is working
 p = Polynomial(22)
 p = p + 2
-print(p)
+expectedString = "(22+2)"
+compare(expectedString, str(p))
 
 p = p * p
-print(p)
+expectedString = "((22+2)*(22+2))"
+compare(expectedString, str(p))
 
 x = Variable("x")
 
 p = x + p * x
-print(p)
+expectedString = "(x+((22+2)*(22+2)*x))"
+compare(expectedString, str(p))
 
 y = Variable("y")
 q = Polynomial(3)
 q = y + q
+expectedString = "(y+q)"
 
-for x in p.vars:
-    print(x)
-print()
+expectedSet = {"x"}
+compareSet(expectedSet, p.vars)
 
-for x in q.vars:
-    print(x)
-print()
+expectedSet = {"y"}
+compareSet(expectedSet, q.vars)
 
-for x in (p + q).vars:
-    print(x)
+expectedSet = {"x", "y"}
+compareSet(expectedSet, (p + q).vars)
+
 
 x = Variable("x")
 y = Variable("y")
-print(x + y)
-print("x+y")
+compare("(x+y)", str(x + y))
 
 p1 = ((2 * y) + x)
 p2 = ((x + y) * 10)
 p3 = (y * x) ** 10
-print(p1)
-print(p2)
 p = p1 * p2 + p3
-print(p)
-print("p")
+expectedString = "(((x+y)*10*((y*2)+x))+((y*x)^10))"
+compare(expectedString, str(p))
+
 
 p = 2 + x + 4 + y
 q = 2 + x + (4 * y)
 r = (2 + x) * (4 * y)
-print(p, q, r)
-print(p + q)
+compare("(x+2+4+y)", str(p))
+compare("(x+2+(y*4))", str(q))
+compare("(y*4*(x+2))", str(r))
 
-print(q)
-print(q.eval({"x": 10, "y": 0}))
-print(q.eval({"x": 10, "y": -3}))
+q1 = q.eval({"x": 10, "y": 0})
+compare(q1, 2)
+q2 = q.eval({"x": 10, "y": -3})
+compare(q1, 1)
 
 print()
 po = (2 + x) * (4 + x)
-print(po)
-print(po.simplify())
+simplified = po.simplify()
+comparePoly(po, simplified)
